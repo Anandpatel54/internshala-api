104887e9c061dbb4eded81039a7a9c66151a9dbb
Anand patel <panand054@gmail.com>
2024-04-21 12:04:21 +0530

    jobs and internship part

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -38,3 +38,67 @@ exports.deleteeducation = catchAsyncErrors(async (req, res, next) => {
     await student.save();
     res.json({ message: "Education deleted!" });
   });
+
+  //---------------jobs---------------------
+
+exports.addjob = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  student.resume.jobs.push({ ...req.body, id: uuidv4() });
+  await student.save();
+  res.json({ message: "job Added!" });
+});
+
+exports.editjob = catchAsyncErrors(async (req, res, next) => {
+  const student = await Student.findById(req.id).exec();
+  const eduIndex = student.resume.jobs.findIndex(
+    (i) => i.id === req.params.eduid
+  );
+  student.resume.jobs[eduIndex] = {
+    ...student.resume.jobs[eduIndex],
+    ...req.body,
+  };
+  await student.save();
+  res.json({ message: "job updated!" });
+});
+
+exports.deletejob = catchAsyncErrors(async (req, res, next) => {
+    const student = await Student.findById(req.id).exec();
+    const filterededu = student.resume.jobs.filter(
+      (i) => i.id !== req.params.eduid
+    );
+    student.resume.jobs = filterededu
+    await student.save();
+    res.json({ message: "job deleted!" });
+  });
+
+  //---------------internship---------------------------
+
+  exports.addinternship = catchAsyncErrors(async (req, res, next) => {
+    const student = await Student.findById(req.id).exec();
+    student.resume.internship.push({ ...req.body, id: uuidv4() });
+    await student.save();
+    res.json({ message: "internship Added!" });
+  });
+  
+  exports.editinternship = catchAsyncErrors(async (req, res, next) => {
+    const student = await Student.findById(req.id).exec();
+    const eduIndex = student.resume.internship.findIndex(
+      (i) => i.id === req.params.eduid
+    );
+    student.resume.internship[eduIndex] = {
+      ...student.resume.internship[eduIndex],
+      ...req.body,
+    };
+    await student.save();
+    res.json({ message: "internship updated!" });
+  });
+  
+  exports.deleteinternship = catchAsyncErrors(async (req, res, next) => {
+      const student = await Student.findById(req.id).exec();
+      const filterededu = student.resume.internship.filter(
+        (i) => i.id !== req.params.eduid
+      );
+      student.resume.internship = filterededu
+      await student.save();
+      res.json({ message: "internship deleted!" });
+    });
--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { resume, addeducation, editeducation, deleteeducation } = require("../controllers/resumeController");
+const {
+  resume,
+  addeducation,
+  editeducation,
+  deleteeducation,
+  addjob,
+  editjob,
+  deletejob,
+  addinternship,
+  editinternship,
+  deleteinternship,
+} = require("../controllers/resumeController");
 const { isAuthenticated } = require("../middlewares/auth");
 
 //get/
@@ -10,9 +21,33 @@ router.get("/", isAuthenticated, resume);
 router.post("/add-adu", isAuthenticated, addeducation);
 
 //POST
-router.post("/edit-edu/:eduid", isAuthenticated, editeducation)
+router.post("/edit-edu/:eduid", isAuthenticated, editeducation);
 
 //POST
-router.post("/delete-edu/:eduid", isAuthenticated, deleteeducation)
+router.post("/delete-edu/:eduid", isAuthenticated, deleteeducation);
+
+//------------------Job-----------------
+
+//POST
+router.post("/add-job", isAuthenticated, addjob);
+
+//POST
+router.post("/edit-job/:eduid", isAuthenticated, editjob);
+
+//POST
+router.post("/delete-job/:eduid", isAuthenticated, deletejob);
+
+  //---------------internship---------------------------
+
+  //POST
+router.post("/add-internship", isAuthenticated, addinternship);
+
+//POST
+router.post("/edit-internship/:eduid", isAuthenticated, editinternship);
+
+//POST
+router.post("/delete-internship/:eduid", isAuthenticated, deleteinternship);
+
+
 
 module.exports = router;
